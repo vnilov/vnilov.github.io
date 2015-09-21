63bf6c563ffc12776c3e55335762730ffca84241
Nilov Vadim <vnilov@ctcmedia.ru>
2015-09-21 16:29:03 +0300

    Initial commit

---
+++ assets/js/src/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vnilov on 9/6/15.
+ */
+
+
+
+
+/* smooth scroll plugin init */
+smoothScroll.init();
+
+
+
+
+/* menu collapse behaviour */
+$('#bd-main-nav').on('show.bs.collapse', function(){ $('.js-nav-right').removeClass('pull-right'); });
+$('#bd-main-nav').on('hidden.bs.collapse', function(){ $('.js-nav-right').addClass('pull-right'); });
+
+$('.js-nav-link').on('click', function(){$('#bd-main-nav').collapse('hide');});
---
+++ for_Rob/index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-s<cale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="author" content="Nilov Vadim">
+    <title>Experience details</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+</head>
+<body>
+<header class="navbar  navbar-dark bg-inverse  navbar-static-top  bd-navbar  navbar-fixed-top" data-scroll-header>
+    <div class="container">
+        <div class="clearfix">
+            <button class="navbar-toggler pull-right hidden-sm-up" data-target="#bd-main-nav" data-toggle="collapse" type="button"> ☰ </button>
+            <a class="navbar-brand hidden-sm-up" href="/">Nilov Vadim</a>
+        </div>
+        <div id="bd-main-nav" class="collapse navbar-toggleable-xs">
+            <nav class="nav navbar-nav ">
+                <a class="nav-item nav-link  js-nav-link" data-scroll href="#backend">Backend experience</a>
+                <a class="nav-item nav-link  js-nav-link" data-scroll href="#frontend">Frontend experience</a>
+                <a class="nav-item nav-link  js-nav-link" data-scroll href="#ui">UI</a>
+                <a class="nav-item nav-link  js-nav-link" data-scroll href="#agile">Agile</a>
+                <a class="nav-item nav-link  js-nav-link" data-scroll href="#more">More</a>
+                <div class="pull-right  js-nav-right">
+                    <a class="nav-item nav-link  js-nav-link" href="mailto:nilov.vadim@gmail.com">Contact Me</a>
+                    <a class="nav-item nav-link  js-nav-link" href="/files/CV_VNilov.pdf" target="_blank">My CV</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
+<main class="m-t-lg  m-b-lg">
+    <div class="container">
+        <div class="row">
+            <div class="jumbotron  col-xs-12 m-t-md">
+                <p class="lead">Hi, Rob!</p>
+                <p>
+                    This page contain my response for you request.
+                    I tried to write it in detail.
+                    And I hope that it will help you to make the right decision.
+                </p>
+            </div>
+        </div>
+    </div>
+    <section id="backend">
+        <div class="container">
+            <h2 class="m-t-lg">Backend experience</h2>
+            <!-- -->
+            <h3 class="m-t  text-info">PHP</h3>
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>
+                        First of all, i want to note you that i have more than 5+ years experience with php.
+                    </p>
+                    <p>
+                        I started to learn it in the university at second semester.
+                        My first project was a site of the soccer championship with client and admin side. It hosted on
+                        the university hosting, i'm not sure that it's working now.
+                    </p>
+                    <p>
+                        I had a lot of experience to write code on php since that time.
+                    </p>
+                    <p>
+                    Here are just several PHP projects with their description in which I participated:
+                    </p>
+                    <div class="row">
+                        <div class="col-sm-4  col-xs-12">
+                            <div class="card  card-block">
+                                <h4 class="card-title">CTCLOVE.RU</h4>
+                                <p class="card-text">
+                                    <strong>Launched:</strong> 2015<br/>
+                                    <strong>Framework:</strong> <a name="bitrix">Bitrix</a><sup>*</sup><br/>
+                                    <strong>Technologies:</strong> PHP, JavaScript (JQuery, AJAX), MySQL, Memcache, HTML, CSS, LESS, SASS(SCSS), Twitter Bootstrap<br/>
+                                    <strong>Role:</strong> full-stack developer (backend, frontend).<br/>
+                                    <strong>About:</strong> tv channel site. Integration with <a href="http://videomore.ru" target="_blank">Videomore</a> video service. Tests, Contests and other users services.<br/>
+                                </p>
+                                <a href="http://ctclove.ru" class="card-link">Link</a>
+                                <a href="https://github.com/vnilov/classes/blob/master/VideomoreApi.php" target="_blank" class="card-link">Class example</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-4  col-xs-12">
+                            <div class="card  card-block">
+                                <h4 class="card-title">CTC.RU (previous version)</h4>
+                                <p class="card-text">
+                                    <strong>Launched:</strong> 2012<br/>
+                                    <strong>Framework:</strong> <a href="#bitrix">Bitrix</a><sup>*</sup><br/>
+                                    <strong>Technologies:</strong> PHP, JavaScript (JQuery, AJAX, AngularJS), MySQL, Memcache, HTML, CSS, LESS, SASS(SCSS), Twitter Bootstrap<br/>
+                                    <strong>Role:</strong> full-stack developer (backend, frontend). Server admin (FeeBSD). NGINX configuration.<br/>
+                                    <strong>About:</strong> tv channel site. Integration with <a href="http://videomore.ru" target="_blank">Videomore</a> video service. Tests, Contests and other users services.<br/>
+                                    <strong>Highload project:</strong> Over 8000 online. 1 backend server configuration.
+                                </p>
+                                <a href="http://old.ctc.ru" class="card-link">Link</a>
+                                <a href="https://github.com/vnilov/classes/blob/master/UnisenderAPI.php" target="_blank" class="card-link">Class example</a>
+                                <a href="https://github.com/vnilov/classes/blob/master/UnisenderAPITest.php" target="_blank" class="card-link">UnitTest example</a>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4  col-xs-12">
+                            <div class="card  card-block">
+                                <h4 class="card-title">SWEETME.RU</h4>
+                                <p class="card-text">
+                                    <strong>Launched:</strong> 2015<br/>
+                                    <strong>Framework:</strong> Symfony2<br/>
+                                    <strong>Technologies:</strong> PHP, JavaScript (JQuery, AJAX), MySQL, Memcache<br/>
+                                    <strong>Role:</strong> support and improvement features of the project<br/>
+                                    <strong>About:</strong> promo site of a dress shop<br/>
+                                </p>
+                                <a href="http://sweetme.ru" class="card-link">Link</a>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4  col-xs-12">
+                            <div class="card  card-block">
+                                <h4 class="card-title">LETIDOR.RU</h4>
+                                <p class="card-text">
+                                    <strong>Launched:</strong> 2011<br/>
+                                    <strong>Framework:</strong> <a href="#bitrix">Bitrix</a><sup>*</sup><br/>
+                                    <strong>Technologies:</strong> PHP, JavaScript (JQuery, AJAX), MySQL, Memcache, HTML, CSS.<br/>
+                                    <strong>Role:</strong> full-stack developer (backend, frontend).<br/>
+                                    <strong>About:</strong> content project about children for their parents. Articles, Tests, Video, Forum, Auction, Pregnancy calendar and other services.<br/>
+                                </p>
+                                <a href="http://letidor.ru" target="_blank" class="card-link">Link</a>
+                                <a href="https://github.com/vnilov/classes/blob/master/CTradeUnit.php" target="_blank" class="card-link">Class example</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- -->
+            <h3 class="text-info">MVC frameworks</h3>
+            <div class="row m-t" >
+                <div class="col-xs-12  col-sm-12">
+                    <h4 id="bitrix">Bitrix*</h4>
+                    <p>
+                        It's a professional shareware MVC framework.
+                        It uses classic MVC architectural pattern.
+                        Bitrix has a little differences with file structure unlike frameworks like Yii or Symfony.
+                        It's very scalable and well documented its classes.
+                    </p>
+                    <h4 id="bitrix">Symfony and Symfony2</h4>
+                    <p>I have work experience with both of this frameworks. I'm supporting sweet.me project, which based on Symfony2.
+                        And I supported previous version of <a href="http://peretz.ru" target="_blank">peretz.ru</a>, which was based on Symfony. </p>
+                    <h4 id="bitrix">Yii</h4>
+                    <p>
+                        I have a little experience with Yii. I like it! I started creating project to delivery company, but they were closed soon and i didn't finish it.
+                        <a href="https://github.com/vnilov/delivery" target="_blank">Project on GitHub</a>
+                    </p>
+                    <h4 id="bitrix">Ruby on Rails</h4>
+                    <p>
+                        I finished three courses on <a href="https://www.codeschool.com" target="_blank">https://www.codeschool.com</a>
+                        Also I studied Ruby in the University for one semester.
+                    </p>
+                </div>
+            </div>
+            <!-- -->
+            <h3 class="text-info">Laravel</h3>
+            <div class="row ">
+                <div class="col-xs-12  col-sm-12">
+                    I haven't got any experience with Laravel.<br/>
+                    I read some materials about this framework, so it's standard MVC framework.
+                    It used ActiveRecord pattern for mysql like yii or symfony. It has route model like Bitrix or Yii or another MVC framework.
+                    I'm ready to use it and i'm sure that it won't give me any troubles.
+                </div>
+            </div>
+            <!-- -->
+            <h3 class="text-info  m-t">MySQL</h3>
+            <div class="row">
+                <div class="col-xs-12  cols-sm-12">
+                    <p>I started learning SQL courses at the University as PHP. We studied PostgeSQL.</p>
+                    <p>
+                        I know what means "Normal form" and I understand difference between 1..6NF or BCNF.
+                        I can read and write simple or different SQL queries.<br/>
+                        I understand where I should or shouldn't add index to a SQL table.
+                    </p>
+                    <p>
+                        I have experience with ActiveRecord pattern. (<a href="https://github.com/vnilov/delivery/blob/master/protected/models/Position.php" target="_blank">Yii model Example</a>)
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section id="frontend">
+        <div class="container">
+            <h2 class="m-t-lg">Frontend experience</h2>
+            <h3 class="text-info  m-t">JQuery</h3>
+            <div class="row">
+                <div class="col-xs-12  col-sm-12">
+                    <p>
+                    I have a lot of experience with JQuery.<br/>
+                    So, I use many JavaScript JQuery libraries.<br/>
+                    The most common ones:
+                        <ul>
+                            <li>Masonry (<a href="http://mband.ctclove.ru/" target="_blank">Usage example [Listing of elements]</a>)</li>
+                            <li>JQueryValidate ( most of forms on all projects )</li>
+                            <li>Smooth Scroll</li>
+                            <li>FastClick ( must add for responsive projects )</li>
+                            <li>Mobile Detect</li>
+                            <li>DropzoneJS ( my favorite library to asynchronous upload files)</li>
+                            <li>JQuery UI</li>
+                            <li>All of Bootstrap JS plugins</li>
+                            <li>Foundation plugins</li>
+                            <li>and more others</li>
+                        </ul>
+                    </p>
+                    <p>Of course I write my own JS. <a href="http://2s.ctc.ru/scripts/main.js" target="_blank">Example</a></p>
+                </div>
+            </div>
+            <h3 class="text-info">Sass</h3>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                    <p>I use SASS (SCSS) in my projects. As example, I use PhpStorm SASS plugin and FileWatcher for SCSS.</p>
+                    <a href="https://github.com/vnilov/homepage/blob/master/www/for_Rob/sass_expl.scss" target="_blank">My SCSS example</a>
+                </div>
+            </div>
+            <h3 class="text-info  m-t">Responsive design</h3>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                    Most of my projects has responsive design.<br/>
+                    In some of them I take the opportunity of the Twitter Bootstrap. (<a href="http://www.ctclove.ru/" target="_blank">Example 1</a>, <a href="http://k.ctc.ru/" target="_blank">Example 2</a>)
+                    In some I use InuitCSS (SASS modules) with its opportunities of responsive (<a href="http://2s.ctc.ru" target="_blank">Example 1</a>, <a href="http://mband.ctclove.ru" target="_blank">Example 2</a>).
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="ui">
+        <div class="container">
+            <h2 class="m-t-lg">UI</h2>
+            <h3 class="text-info  m-t">Basic UI design</h3>
+            <div class="row">
+                <div class="col-xs-12  col-sm-12">
+                    <p>
+                        Nowadays we have many instruments for help us with it: many frontend frameworks with their grids and other instruments,
+                        guidelines (material design for example).
+                    </p>
+                </div>
+            </div>
+            <h3 class="text-info">Skills in Photoshop</h3>
+            <div class="row">
+                <div class="col-xs-12  col-sm-12">
+                    <p>
+                        I have basic skills in Photoshop.
+                        I can use standard functions: optimizing images, save images for web, change canvas size, use guidelines, drop shadows, cut layers, add layer functions, etc...
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="agile">
+        <div class="container">
+            <h2 class="m-t-lg">Agile</h2>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                    Now I practice Extreme Programming methodology at my work. As you know it's one of agile methodologies.
+                    But when I worked in <a href="http://qsoftus.com/" target="_blank">QSoft</a> I worked in <strong>SCRUM team</strong>.
+                    We had 1 week sprints in it.
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="more">
+        <div class="container">
+            <h2 class="m-t-lg">More</h2>
+            <h3 class="m-t  text-info">VCS</h3>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                    I use Git. I try to use good naming in my commits and tags.
+                    I have experience with SVN.
+                </div>
+            </div>
+            <h3 class="m-t  text-info">CSS guidelines and BEM methodology</h3>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                    I'm familiar with BEM methodology by Yandex.
+                    I studied cssguidelines by Harry Roberts and i try to follow.
+                    My favorite css framework is <a href="https://github.com/inuitcss" target="_blank">InuitCSS</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<footer class="navbar navbar-dark bg-inverse">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12  col-sm-6">
+                <p>Please give me feedback anyway: <a href="mailto:nilov.vadim@gmail.com">nilov.vadim@gmail.com</a></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12  col-sm-12">
+                <p>This page I created by <a href="http://v4-alpha.getbootstrap.com/" target="_blank">Twitter Bootstrap 4 Alpha</a>. It will be very good framework I think.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script type="application/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script type="application/javascript" src="/assets/js/vendor/bootstrap.js"></script>
+<script type="application/javascript" src="/assets/js/vendor/smooth-scroll.min.js"></script>
+<script type="application/javascript" src="/assets/js/src/main.js"></script>
+</body>
+</html>
