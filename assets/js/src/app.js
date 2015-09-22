f5fd166684cd27a5dd0fd6002d9fb9afd0852c69
vnilov <nilov.vadim@gmail.com>
2015-09-23 00:09:10 +0300

    header (menu) about card

---
+++ assets/js/src/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by vnilov on 9/22/15.
+ */
+
+    /* menu */
+    $('.js-hamburger').on('click', function(e){
+        e.preventDefault();
+        var activeClass = 'main-menu--active';
+        if ($('.js-main-menu').hasClass(activeClass)) {
+            $('.js-main-menu').removeClass(activeClass);
+        } else {
+            $('.js-main-menu').addClass(activeClass);
+        }
+    });
--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -11,8 +11,12 @@ smoothScroll.init();
 
 
 
-/* menu collapse behaviour */
+/* menu for_Rob collapse behaviour */
 $('#bd-main-nav').on('show.bs.collapse', function(){ $('.js-nav-right').removeClass('pull-right'); });
 $('#bd-main-nav').on('hidden.bs.collapse', function(){ $('.js-nav-right').addClass('pull-right'); });
 
 $('.js-nav-link').on('click', function(){$('#bd-main-nav').collapse('hide');});
+
+
+
+
