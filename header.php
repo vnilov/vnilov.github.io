a7d538d72f7071ec5fd8f7ed24fcb18d2ca4364f
vnilov <nilov.vadim@gmail.com>
2016-08-21 23:04:30 +0300

    Add sandbox page

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <meta name="keywords" content="Нилов Вадим, Nilov Vadim, PHP developer, PHP, backend, backend developer, frontend, frontend developer" />
+    <meta name="description" content="Personal site of Nilov Vadim, php developer" />
+    <title>Vadim Nilov's homepage</title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/assets/css/materialdesignicons.min.css">
+    <?php if ($_SERVER['HTTP_HOST'] == 'lookover.me'):?>
+        <link rel="stylesheet" href="/assets/css/styles_grey.css">
+    <?php else: ?>
+        <link rel="stylesheet" href="/assets/css/styles.css">
+    <?php endif;?>
+</head>
+<body class="background--main">
+<div class="header">
+    <div class="container">
+        <div class="layout  layout--middle">
+            <div class="layout__item  u-1/4  u-3/4-palm   u-1/4-lap">
+                <div class="brand">
+Vadim Nilov
+</div>
+            </div><!--
+         --><div class="hamburger  js-hamburger  layout__item  u-1/4-palm"><i class="mdi mdi-menu"></i></div><!--
+         --><div class="main-menu  js-main-menu  layout__item  u-3/4  u-1/1-palm  u-3/4-lap">
+                <ul class="list-inline">
+                    <li class="main-menu__item"><a href="#aboutMe" data-scroll>About me</a></li>
+                    <li class="main-menu__item"><a href="#workExperience" data-scroll>Work Experience</a></li>
+                    <li class="main-menu__item"><a href="#skills" data-scroll>Skills</a></li>
+                    <li class="main-menu__item"><a href="#follow" data-scroll>Follow me</a></li>
+                    <li class="main-menu__item"><a href="mailto:nilov.vadim@gmail.com">Hire me</a></li>
+                    <li class="main-menu__item"><a href="/sandbox">Sandbox</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="">
+    <div class="container">
